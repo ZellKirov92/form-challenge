c68148309d226364446b78e0a270f23edbe01a7b
ZellKirov92 <gwen.dragonrouge@gmail.com>
2024-03-07 19:05:13 +0100

    modif index.html for add span for display error modif style add style displayError modif js function for display error via span message

--- js/noValidate.js
+++ js/noValidate.js
@@ -1,11 +1,12 @@
 export function noValidate () {
-        const checkFname = document.getElementById("fname");
-        checkFname.classList.add("invalid");
-        let checkLname = document.getElementById("lname");
-        checkLname.classList.add("invalid");
-        let checkEmail = document.getElementById("email");
-        checkEmail.classList.add("invalid");
-        let checkPassword = document.getElementById("password");
-        checkPassword.classList.add("invalid");
+        const inputsId = ['fname', 'lname', 'email', 'password'];
+        for( const id of inputsId ) {
+            const allIdInput = document.getElementById(id);
+            allIdInput.classList.add('invalid');
+        }
+        const allSpan = document.querySelectorAll("span.displayError");
+        allSpan.forEach((span) => {
+            span.style.display = "block";
+        });
         return false;
     }
--- js/validate.js
+++ js/validate.js
@@ -1,11 +1,13 @@
 export function validate() {
-        let checkFname = document.getElementById("fname");
-        checkFname.classList.remove("invalid");
-        let checkLname = document.getElementById("lname");
-        checkLname.classList.remove("invalid");
-        let checkEmail = document.getElementById("email");
-        checkEmail.classList.remove("invalid");
-        let checkPassword = document.getElementById("password");
-        checkPassword.classList.remove("invalid");
+        const inputsId = ['fname', 'lname', 'email', 'password'];
+        for( const id of inputsId ) {
+            const allIdInput = document.getElementById(id);
+            allIdInput.classList.remove('invalid');
+        }
+        const allSpan = document.querySelectorAll('span.displayError');
+        allSpan.forEach((span) => {
+            span.style.display = "none";
+        });
+
         return true;
 }
